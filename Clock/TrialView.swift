1fe0b03c352ca988c9d7de10f7ff7d4e9c8be520
Max Valek <valek.maxwell@gmail.com>
2023-09-09 19:06:49 -0500

    gradient pie

---
+++ Clock/TrialView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                Circle()
+                    .fill(.ultraThinMaterial)
+                    .background {
+                        GeometryReader { proxy in
+                            Path { path in
+                                let center = CGPoint(x: proxy.size.width / 2, y: proxy.size.height / 2)
+                                let radius = min(proxy.size.width, proxy.size.height) / 2.0
+
+                                path.move(to: center)
+                                path.addArc(center: center, radius: radius, startAngle: .degrees(0), endAngle: .degrees(270), clockwise: false)
+                                path.closeSubpath()
+                            }
+                            .fill(
+                                AngularGradient(
+                                    colors: [.red, .orange, .yellow, .green],
+                                    center: .center,
+                                    startAngle: .degrees(0),
+                                    endAngle: .degrees(270))
+                            )
+                            .rotationEffect(.degrees(-90))
+                        }
+                    }
+                    .blendMode(.screen)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
